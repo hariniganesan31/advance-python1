19fa7dd80e68e290b77f264b113208cc60f0803d
hariniganesan31 <harinig3101@gmail.com>
2026-01-22 13:59:02 +0530

    Add files via upload

---
+++ if else tp.py
@@ -0,0 +1,45 @@
+'''n=5
+if(n>0):
+    print("n is positive")
+elif(n==0):
+    print("n is zero")
+else:
+    print("n is negative")'''
+
+
+'''n=7
+if(n%2==0):
+   print("even")
+else:
+   print("odd")'''
+
+'''a=10
+b=20
+if(a>b):
+    print("a is greater")
+else:
+    print("b is greater")'''
+
+'''age=19
+if(age>=18):
+    print("eligiable for vote")
+else:
+    print("not eligiable")'''
+
+'''for i in range(1,11):
+    print(i)'''
+
+'''for i in range(2,21,2):
+    print(i)'''
+
+'''i = 1
+while i <= 10:
+    print(i)
+    i += 1'''
+
+n=int(input("enter"))
+total=0
+for i in range(n+1):
+    total+=i
+    print(total)
+
